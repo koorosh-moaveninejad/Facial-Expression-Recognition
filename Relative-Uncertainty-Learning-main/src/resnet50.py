33d8fd8897cee2afa841561403819bda6e9c23cd
koorosh moaven <km.nejad@yahoo.com>
2026-05-05 12:45:20 +0200

    add ResNet-50

---
+++ Relative-Uncertainty-Learning-main/src/resnet50.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+import torch
+import torch.nn as nn
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=False):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+
+        self.conv2 = nn.Conv2d(
+            out_channels,
+            out_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.conv3 = nn.Conv2d(
+            out_channels,
+            out_channels * self.expansion,
+            kernel_size=1,
+            bias=False
+        )
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        if downsample:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(
+                    in_channels,
+                    out_channels * self.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False
+                ),
+                nn.BatchNorm2d(out_channels * self.expansion)
+            )
+        else:
+            self.downsample = None
+
+    def forward(self, x):
+        identity = x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+
+        return x
+
+
+class ResNet50(nn.Module):
+    def __init__(self, block=Bottleneck, n_blocks=[3, 4, 6, 3],
+                 channels=[64, 128, 256, 512], output_dim=1000):
+        super().__init__()
+
+        self.in_channels = channels[0]
+
+        self.conv1 = nn.Conv2d(
+            3, self.in_channels,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self.get_resnet_layer(block, n_blocks[0], channels[0])
+        self.layer2 = self.get_resnet_layer(block, n_blocks[1], channels[1], stride=2)
+        self.layer3 = self.get_resnet_layer(block, n_blocks[2], channels[2], stride=2)
+        self.layer4 = self.get_resnet_layer(block, n_blocks[3], channels[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(self.in_channels, output_dim)
+
+    def get_resnet_layer(self, block, n_blocks, channels, stride=1):
+        layers = []
+
+        if self.in_channels != block.expansion * channels or stride != 1:
+            downsample = True
+        else:
+            downsample = False
+
+        layers.append(block(self.in_channels, channels, stride, downsample))
+
+        self.in_channels = block.expansion * channels
+
+        for _ in range(1, n_blocks):
+            layers.append(block(self.in_channels, channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        h = x.view(x.shape[0], -1)
+        x = self.fc(h)
+
+        return x, h
+
+
+if __name__ == '__main__':
+    model = ResNet50(output_dim=1000)
+    input_tensor = torch.randn(1, 3, 224, 224)
+    output, features = model(input_tensor)
+
+    print("Output shape:", output.shape)
+    print("Feature shape:", features.shape)
+
+    
