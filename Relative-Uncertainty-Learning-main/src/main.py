cbc709748a1af2f9fade59c8afcf0119348ec1d1
koorosh moaven <km.nejad@yahoo.com>
2026-05-14 17:09:39 +0200

    enhance augmentation

--- Relative-Uncertainty-Learning-main/src/main.py
+++ Relative-Uncertainty-Learning-main/src/main.py
@@ -87,12 +87,20 @@ def train():
     fc = nn.Linear(args.out_dimension, 7)
 
     data_transforms = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
-        transforms.RandomErasing(scale=(0.02, 0.25))
+    transforms.ToPILImage(),
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(10),
+    transforms.ColorJitter(
+        brightness=0.2,
+        contrast=0.2,
+        saturation=0.2,
+        hue=0.05
+    ),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
+    transforms.RandomErasing(scale=(0.02, 0.30), p=0.5)
     ])
 
     data_transforms_val = transforms.Compose([
